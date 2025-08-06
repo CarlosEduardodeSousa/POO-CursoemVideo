39303597fb68ed3e3a184b277014ca2de48d8beb
Carlos Eduardo de Sousa <carloseduardosousa378@gmail.com>
2025-08-06 20:25:05 -0300

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+    public static void main(String[] args) {
+         
+        Retangulo r = new Retangulo();
+
+        r.setAltura(5);
+        r.setBase(4);
+        r.calcularArea();
+        r.calcularPerimetro();
+        r.status();
+    }
+}
